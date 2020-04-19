5d0b9ad07a1085f21fc1b3ab8e41abc57c245484
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2020-04-19 18:22:01 -0300

    finished

--- src/resources/js/globals.js
+++ src/resources/js/globals.js
@@ -1,5 +1,5 @@
 const globals = {
-  auth: "563492ad6f91700001000001c0abf84fcee04fd6ab4246ecd48c7c28",
+  auth: "KEY API HERE",
   gallery: document.querySelector("article"),
   form: document.querySelector("form"),
   searchInput: document.querySelector("input"),
